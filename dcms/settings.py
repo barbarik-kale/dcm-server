bdd8f484ba6893c904e92f0f9bcd12b0124f681d
barbarik-kale <barbarik.kale@gmail.com>
2024-10-11 23:17:06 +0530

    include urls, add settings and requirements

--- dcms/settings.py
+++ dcms/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'api.middleware.JsonBodyMiddleware',
 ]
 
 ROOT_URLCONF = 'dcms.urls'
